115e6f10ec6f94352274200914e7c1332cc2c078
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-05-20 14:44:55 -0500

    Queue intro, Constructor and a print function

--- Stack.py
+++ Stack.py
@@ -8,7 +8,8 @@ class Stack:
     def __init__(self,value):
         new_node = Node (value)
         self.top = new_node
-        self.height = 1
+        self.height =+ 1
+
 
     def print_stack(self):
         temp = self.top
@@ -25,6 +26,20 @@ class Stack:
             self.top = new_node
         self.height += 1
 
+    def pop (self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp.value
+
+
 my_stack = Stack (4)
 my_stack.push(45)
+my_stack.push(33)
+
+print(my_stack.pop() )
+
 my_stack.print_stack()
