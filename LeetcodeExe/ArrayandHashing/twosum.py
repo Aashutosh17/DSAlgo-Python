7717c648e7ea56692296f574d3daf8097db12a3f
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-05-31 17:08:52 -0500

    longest common prefix

--- LeetcodeExe/ArrayandHashing/twosum.py
+++ LeetcodeExe/ArrayandHashing/twosum.py
@@ -1,8 +1,14 @@
+from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        for index1 in range (n  -1):
+        for index1 in range (n):
             for index2 in range ( index1 + 1, n):
                 if nums[index1] + nums[index2] == target:
                     return [index1, index2]
         return None
+
+result = Solution()
+Solution.twoSum([2,3,4,5], 5)
