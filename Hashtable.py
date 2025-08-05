c618de95d7ed14843db244fc9edae2dd78ffe3ab
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-08-05 16:14:05 -0500

    HashTable get_items

--- Hashtable.py
+++ Hashtable.py
@@ -20,9 +20,23 @@ class Hashtable:
             self.data_map[index] = [] # initializing Exmpty list
         self.data_map[index].append([key,value])
 
+    # Get
+    def get_item(self,key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range (len(self.data_map[index])):
+                if self.data_map[index] [i][0] == key:
+                    return self.data_map[index] [i] [1]
+        return None
+
 
 my_hash_table = Hashtable()
 my_hash_table.set_item('bolts',140)
 my_hash_table.set_item('nails',200)
 my_hash_table.set_item('Paint',500)
+
+print(my_hash_table.get_item('nails'))
+print(my_hash_table.get_item('Washers'))
+print(my_hash_table.get_item('bolts'))
+
 my_hash_table.print_table()
