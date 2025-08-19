454468c947e1f09df014badf248216b3ca4cf259
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-08-18 21:42:52 -0500

    Heap: Helper Method

--- Graphs.py
+++ Graphs.py
@@ -2,15 +2,18 @@ class Graph:
     def __init__(self):
         self.adj_list = {}
 
+
     def print_graph(self):
         for vertex in self.adj_list:
             print(vertex, ':', self.adj_list[vertex])
+
 
     def add_vertex(self,vertex):
         if vertex not in self.adj_list.keys():
             self.adj_list[vertex]= []
             return True 
         return False
+
 
     def add_edge(self,v1,v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
@@ -19,6 +22,7 @@ class Graph:
 
             return True
         return False
+
 
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
@@ -29,6 +33,7 @@ class Graph:
                 pass
             return True
         return False
+
 
     def remove_vertex(self,vertex):  # remove edge then vertex
         if vertex in self.adj_list.keys():
