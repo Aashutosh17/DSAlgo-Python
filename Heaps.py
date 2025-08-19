446522d0701ae9847e25b3a7ee59abcc66c67962
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-08-18 22:35:45 -0500

    Heap: Remove and Sink down method

--- Heaps.py
+++ Heaps.py
@@ -1,31 +1,62 @@
 class MaxHeap:
     def __init__(self):
-        self.heap =[]
+        self.heap = []
 
-    def _left_child(self,index):
+    # For any element at 'index':
+    # left child = 2 * index + 1
+    # right child = 2 * index + 2
+    # These formulas come from how a binary tree is stored in an array
+    def _left_child(self, index):
         return 2 * index + 1
 
-    def _right_child(self,index):
+    def _right_child(self, index):
         return 2 * index + 2
 
-    def _parent(self,index):
+    # Parent of a node is found by (index - 1) // 2
+    def _parent(self, index):
         return (index - 1) // 2
 
     def _swap(self, index1, index2):
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
-# These were our helper method
-# Now Heap Insert
-
-    def insert(self,value):
+    # Insert value and bubble it up to maintain max-heap property
+    def insert(self, value):
         self.heap.append(value)
         current = len(self.heap) - 1
-
-        while current > 0 and self.heap[current] >self.heap[self._parent(current)]:
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
+    # Move an element down until the heap property is fixed
+    def _sink_down(self, index):
+        while True:
+            max_index = index
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
 
+            if left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]:
+                max_index = left_index
+
+            if right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]:
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+
+    # Remove and return max (root), then fix heap
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return max_value
 
 
 myheap = MaxHeap()
@@ -33,21 +64,19 @@ myheap.insert(99)
 myheap.insert(72)
 myheap.insert(61)
 myheap.insert(58)
-
-print(myheap.heap)
+""" 
+print(myheap.heap)  # [99, 72, 61, 58] this is our output
 
 myheap.insert(100)
-
-print(myheap.heap)
+print(myheap.heap)  # [100, 99, 61, 58, 72] After inserting 100
 
 myheap.insert(75)
+print(myheap.heap) """ # [100, 99, 75, 58, 72, 61] After inserting 75 we get this.
 
+
+myheap.remove()
 print(myheap.heap)
-
-"""
-Expected Output: 
-----------------
-[99, 72, 61, 58]  # first print heap 
-[100, 99, 61, 58, 72] # when we insert 100 
-[100, 99, 75, 58, 72, 61] # when we insert 75
-"""
+myheap.remove()
+print(myheap.heap)
+myheap.remove()
+print(myheap.heap)
